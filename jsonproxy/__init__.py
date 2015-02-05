eba025c9e0ef97bd71014c5469c8fec80f9b64de
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-02-05 18:27:41 +0100

    Revert "load config from local and home"
    This reverts commit 8d29fede8e07a093ca028d82d7b2a5a6b7ae8b9a.

--- jsonproxy/__init__.py
+++ jsonproxy/__init__.py
@@ -37,33 +37,21 @@ def check_config(config):
 
 def main():
 	parser = argparse.ArgumentParser(description='simple proxy and scraper')
-	parser.add_argument('-c', '--config')
+	parser.add_argument('config')
 	parser.add_argument('-d', '--debug', action='store_true')
 	parser.add_argument('-p', '--port', type=int)
 	parser.add_argument('-H', '--host')
 	args = parser.parse_args()
 
 	app = Flask(__name__)
+	app.config.from_pyfile(os.path.abspath(args.config))
 
-	# load config
-	config_files = [
-		os.path.expanduser('~/.config/pyjsonproxy.cfg'),
-		os.path.abspath('.pyjsonproxy.cfg'),
-	]
-	for path in config_files:
-		if os.path.exists(path):
-			app.config.from_pyfile(path)
-	if args.config is not None:
-		app.config.from_pyfile(os.path.abspath(args.config))
-
-	# check for config errors
 	errors = check_config(app.config)
 	if errors:
 		for error in errors:
 			app.logger.error(error)
 		sys.exit(1)
 
-	# run
 	app.register_blueprint(api)
 	app.run(host=args.host, port=args.port, debug=args.debug)
 
