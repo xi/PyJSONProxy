901b5090d4e4d995221d08e95aed7342fc20c89f
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-02-05 18:58:12 +0100

    Gardening

--- jsonproxy/api.py
+++ jsonproxy/api.py
@@ -1,8 +1,8 @@
 try:
-	from urllib.request import urlopen
+	from urllib.request import urlopen as _urlopen
 	from urllib.error import HTTPError
 except ImportError:
-	from urllib2 import urlopen
+	from urllib2 import urlopen as _urlopen
 	from urllib2 import HTTPError
 
 from flask import Blueprint
@@ -18,6 +18,23 @@ from bs4 import BeautifulSoup
 api = Blueprint('api', __name__, static_folder='static')
 
 
+def urlopen(url, parse=False):
+	try:
+		current_app.logger.info('fetching %s' % url)
+		original = _urlopen(url)
+
+		body = original.read()
+		code = original.getcode()
+		headers = original.headers.items()
+
+		if parse:
+			return BeautifulSoup(body)
+		else:
+			return body, code, headers
+	except HTTPError as error:
+		abort(error.code)
+
+
 def get_attribute_list(html, selector):
 	if '@' in selector:
 		s, attr = selector.rsplit('@', 1)
@@ -28,6 +45,51 @@ def get_attribute_list(html, selector):
 
 def get_attribute(html, selector):
 	return get_attribute_list(html, selector)[0]
+
+
+def scrape_item(url, config):
+	tree = urlopen(url, parse=True)
+	data = {
+		'url': url
+	}
+	for key, selector in config['fields'].items():
+		data[key] = get_attribute(tree, selector)
+	return jsonify(data)
+
+
+def scrape_list(url, config):
+	tree = urlopen(url, parse=True)
+	return jsonify({
+		'url': url,
+		'l': get_attribute_list(tree, config['selector'])
+	})
+
+
+def proxy(url, config):
+	return make_response(*urlopen(url))
+
+
+@api.route('/<endpoint>/<path:path>', methods=['GET'])
+def main(endpoint, path):
+	try:
+		config = current_app.config['ENDPOINTS'][endpoint]
+	except KeyError:
+		abort(404)
+
+	url = request.url.replace(request.host_url + endpoint + '/', config['host'])
+	_type = config.get('type', 'proxy')
+
+	if _type == 'scrape_item':
+		response = scrape_item(url, config)
+	elif _type == 'scrape_list':
+		response = scrape_list(url, config)
+	else:
+		response = proxy(url, config)
+
+	if current_app.config.get('ALLOW_CORS', False):
+		response.headers['Access-Control-Allow-Origin'] = '*'
+
+	return response
 
 
 def _doc(endpoint):
@@ -58,7 +120,7 @@ def _doc(endpoint):
 
 
 @api.route('/', methods=['GET'])
-def main():
+def index():
 	data = [_doc(endpoint) for endpoint in current_app.config['ENDPOINTS']]
 	return render_template('index.html', endpoints=data)
 
@@ -68,46 +130,3 @@ def doc(endpoint):
 	if endpoint not in current_app.config['ENDPOINTS']:
 		abort(404)
 	return render_template('index.html', endpoints=[_doc(endpoint)])
-
-
-@api.route('/<endpoint>/<path:path>', methods=['GET'])
-def proxy(endpoint, path):
-	try:
-		config = current_app.config['ENDPOINTS'][endpoint]
-	except KeyError:
-		abort(404)
-
-	try:
-		url = request.url.replace(request.host_url + endpoint + '/', config['host'])
-		current_app.logger.info('fetching %s' % url)
-		original = urlopen(url)
-	except HTTPError as error:
-		abort(error.code)
-
-	body = original.read()
-	code = original.getcode()
-	headers = original.headers.items()
-
-	type = config.get('type', 'proxy')
-
-	if type == 'scrape_item':
-		html = BeautifulSoup(body)
-		data = {
-			'url': url
-		}
-		for key, selector in config['fields'].items():
-			data[key] = get_attribute(html, selector)
-		response = jsonify(data)
-	elif type == 'scrape_list':
-		html = BeautifulSoup(body)
-		response = jsonify({
-			'url': url,
-			'l': get_attribute_list(html, config['selector'])
-		})
-	else:
-		response = make_response(body, code, headers)
-
-	if current_app.config.get('ALLOW_CORS', False):
-		response.headers['Access-Control-Allow-Origin'] = '*'
-
-	return response
